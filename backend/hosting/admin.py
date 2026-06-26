bd151f84131aa8f0335c41591ccbe3c2c759174c
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-26 12:39:47 +0530

    Update admin.py

--- backend/hosting/admin.py
+++ backend/hosting/admin.py
@@ -1,12 +1,49 @@
 from django.contrib import admin
 from .models import *
 
+
+class PartyPosterInline(admin.TabularInline):
+    model = PartyPoster
+    extra = 1
+
+
 @admin.register(Party)
 class PartyAdmin(admin.ModelAdmin):
-    list_display = ('party_id', 'party_name', 'created_at')
-    search_fields = ('party_id', 'party_name')
+    list_display = (
+        "party_name",
+        "user",
+        "party_category",
+        "party_date",
+        "party_time",
+        "max_members",
+        "fee",
+        "gender",
+    )
 
-@admin.register(Partyposter)
-class PartyposterAdmin(admin.ModelAdmin):
-    list_display = ('poster_id', 'party')
-    search_fields = ('poster_id', 'party')
+    list_filter = (
+        "party_category",
+        "gender",
+        "party_date",
+    )
+
+    search_fields = (
+        "party_name",
+        "location",
+        "description",
+        "user__username",
+    )
+
+    ordering = (
+        "party_date",
+        "party_time",
+    )
+
+    inlines = [PartyPosterInline]
+
+
+@admin.register(PartyPoster)
+class PartyPosterAdmin(admin.ModelAdmin):
+    list_display = (
+        "poster_id",
+        "party",
+    )
