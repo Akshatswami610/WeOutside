200118d64e6be94a6e54d4604f615d937c01cda8
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-26 12:19:27 +0530

    Update admin.py

--- backend/hosting/admin.py
+++ backend/hosting/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('party_id', 'party_name')
+    search_fields = ('party_id', 'party_name')
+
+@admin.register(Partyposter)
+class PartyposterAdmin(admin.ModelAdmin):
+    list_display = ('poster_id', 'party')
+    search_fields = ('poster_id', 'party')
