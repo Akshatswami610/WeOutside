1ad51a9ff66093a3893a901eb84444ed6abb275e
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-26 10:47:04 +0530

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,13 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home
+from .views import login, register, home, hosting, eventdetails, bookingcheckout, about, careers, terms, profile, privacy, support
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     #frontend pages
     path('', home, name='home'),
-    path('home', home, name='home'),
-
+    path('home/', home, name='home'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('hosting/', hosting, name='hosting'),
+    path('eventdetails/', eventdetails, name='eventdetails'),
+    path('bookingcheckout/', bookingcheckout, name='bookingcheckout'),
+    path('profile/', profile, name='profile'),
+    path('careers/', careers, name='careers'),
+    path('privacy/', privacy, name='privacy'),
+    path('support/', support, name='support'),
+    path('about/', about, name='about'),
+    path('terms/', terms, name='terms'),
+    path('privacy/', privacy, name='privacy'),
 ]
