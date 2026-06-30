9486fa7ad5c79551978effaff3dca8a4e39e7c3b
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-30 14:58:24 +0530

    Update models.py

--- backend/hosting/models.py
+++ backend/hosting/models.py
@@ -4,7 +4,7 @@ from datetime import time
 
 User = settings.AUTH_USER_MODEL
 
-class PartyCategory(models.TextChoices):
+class EventCategory(models.TextChoices):
     HOUSE_PARTY = "HOUSE_PARTY", "House Party"
     DINNER_PARTY = "DINNER_PARTY", "Dinner Party"
     GAME_NIGHT = "GAME_NIGHT", "Game Night"
@@ -25,39 +25,47 @@ class PartyCategory(models.TextChoices):
     OPEN_MIC = "OPEN_MIC", "Open Mic"
     OTHER = "OTHER", "Other"
 
+
 class Gender(models.TextChoices):
     ANY = "ANY", "Any"
     MALE = "M", "Male"
     FEMALE = "F", "Female"
     OTHER = "O", "Other"
 
-class Party(models.Model):
-    party_id = models.AutoField(primary_key=True)
+
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    party_name = models.CharField(max_length=100)
-    party_category = models.CharField(max_length=100, choices=PartyCategory.choices, db_index=True)
+
+    event_name = models.CharField(max_length=100)
+    event_category = models.CharField(
+        max_length=100,
+        choices=EventCategory.choices,
+        db_index=True
+    )
     description = models.TextField()
+
+    poster = models.ImageField(upload_to="event_posters/")
+
     max_members = models.PositiveIntegerField()
     fee = models.PositiveIntegerField(default=0)
-    gender = models.CharField(max_length=10, choices=Gender.choices, default=Gender.ANY)
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices,
+        default=Gender.ANY
+    )
     age_limit = models.PositiveIntegerField()
+
     location = models.CharField(max_length=255)
-    party_date = models.DateField(db_index=True)
-    party_time = models.TimeField(default=time(22, 0))
+
+    event_date = models.DateField(db_index=True)
+    event_time = models.TimeField(default=time(22, 0))
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["party_date", "party_time"]
+        ordering = ["event_date", "event_time"]
 
     def __str__(self):
-        return self.party_name
-
-
-class PartyPoster(models.Model):
-    poster_id = models.AutoField(primary_key=True)
-    party = models.ForeignKey( Party, on_delete=models.CASCADE, related_name="posters")
-    poster = models.ImageField(upload_to="party_posters/")
-
-    def __str__(self):
-        return f"{self.party.party_name} Poster"
+        return self.event_name
