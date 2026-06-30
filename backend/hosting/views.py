2c990c594b71774fe08916926211e3c418564cac
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-30 14:58:31 +0530

    Update views.py

--- backend/hosting/views.py
+++ backend/hosting/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class EventListCreateView(generics.ListCreateAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Event.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = "event_id"
+
+    def get_queryset(self):
+        return Event.objects.all()
