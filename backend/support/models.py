db000e8adbd06dc7e3dc171a37d6f03396aa6187
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-30 15:10:46 +0530

    Update models.py

--- backend/support/models.py
+++ backend/support/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Support(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('resolved', 'Resolved'),
+    ]
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
