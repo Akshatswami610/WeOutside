64905c252a2f96ab6d0f813b78d0cdb6232465d0
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-30 11:10:47 +0530

    Create serializers.py

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,67 @@
+from datetime import date
+
+from rest_framework import serializers
+from .models import User
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8, style={"input_type": "password"})
+    confirm_password = serializers.CharField(write_only=True, style={"input_type": "password"})
+
+    class Meta:
+        model = User
+        fields = [
+            "name",
+            "email",
+            "phone_number",
+            "date_of_birth",
+            "password",
+            "confirm_password",
+        ]
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return value
+
+    def validate_phone_number(self, value):
+        if User.objects.filter(phone_number=value).exists():
+            raise serializers.ValidationError("Phone number already exists.")
+        return value
+
+    def validate_date_of_birth(self, value):
+        today = date.today()
+
+        age = today.year - value.year - (
+            (today.month, today.day) < (value.month, value.day)
+        )
+
+        if age < 18:
+            raise serializers.ValidationError(
+                "You must be at least 18 years old to register."
+            )
+
+        return value
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError(
+                {"confirm_password": "Passwords do not match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("confirm_password")
+
+        return User.objects.create_user(
+            name=validated_data["name"],
+            email=validated_data["email"],
+            phone_number=validated_data["phone_number"],
+            date_of_birth=validated_data["date_of_birth"],
+            password=validated_data["password"],
+        )
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True,style={"input_type": "password"})
