f188fdcac209f4d4a099c9b8653f63d772f32a94
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-30 10:38:54 +0530

    Update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number','is_staff')
+    search_fields = ('name', 'email', 'phone_number','is_staff')
+    list_filter = ('name', 'email', 'phone_number','is_staff')
