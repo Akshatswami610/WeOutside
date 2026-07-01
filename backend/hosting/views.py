b65e21610aebb7c7cba648007a16c6812f0c9983
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-01 10:31:14 +0530

    Update views.py

--- backend/hosting/views.py
+++ backend/hosting/views.py
@@ -1,14 +1,33 @@
 from rest_framework import generics, permissions
+from rest_framework.permissions import BasePermission
 from .models import Event
 from .serializers import EventSerializer
 
 
+class IsOwnerOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions for everyone
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions only for the owner
+        return obj.user == request.user
+
+
 class EventListCreateView(generics.ListCreateAPIView):
     serializer_class = EventSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Event.objects.all()
+
+    def get_permissions(self):
+        # Anyone can view events
+        if self.request.method == "GET":
+            return [permissions.AllowAny()]
+
+        # Only authenticated users can host(create) events
+        return [permissions.IsAuthenticated()]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -16,8 +35,14 @@ class EventListCreateView(generics.ListCreateAPIView):
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    queryset = Event.objects.all()
     lookup_field = "event_id"
+    permission_classes = [IsOwnerOrReadOnly]
 
-    def get_queryset(self):
-        return Event.objects.all()
+    def get_permissions(self):
+        # Anyone can view a single event
+        if self.request.method == "GET":
+            return [permissions.AllowAny()]
+
+        # Login required for update/delete
+        return [permissions.IsAuthenticated(), IsOwnerOrReadOnly()]
