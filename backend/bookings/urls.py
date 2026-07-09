86be1de7f4f2f2e05ca6f7ab2f6219ad6806eed8
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-09 11:19:47 +0530

    Update urls.py

--- backend/bookings/urls.py
+++ backend/bookings/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BookingListCreateView, BookingDetailView, EventAttendeesView
+from .views import  BookingListCreateView, BookingDetailView, EventAttendeesView, CreateOrderView, VerifyPaymentView, CancelBookingView
 
 urlpatterns = [
-    path("", BookingListCreateView.as_view(), name="booking-list"),
-    path("<int:pk>/", BookingDetailView.as_view(),name="booking-detail"),
-    path("event/<int:event_id>/attendees/", EventAttendeesView.as_view(), name="event-attendees"),
-
+    path( "", BookingListCreateView.as_view(), name="booking-list" ),
+    path( "<int:pk>/", BookingDetailView.as_view(), name="booking-detail" ),
+    path( "create-order/", CreateOrderView.as_view(), name="create-order" ),
+    path( "verify-payment/", VerifyPaymentView.as_view(), name="verify-payment" ),
+    path( "<int:booking_id>/cancel/", CancelBookingView.as_view(), name="cancel-booking" ),
+    path( "event/<int:event_id>/attendees/", EventAttendeesView.as_view(), name="event-attendees" ),
 ]
