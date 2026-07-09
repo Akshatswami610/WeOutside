8235811bf180d5c8b785e99bc0c47a45d516e234
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-09 22:19:11 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -109,3 +109,12 @@ TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
